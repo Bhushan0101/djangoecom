056f59d85abe3a46c2be96b6171329c578211ee2
Bhushan Bust <bhushanbust@Bhushans-MacBook-Air.local>
2021-05-12 22:17:52 +0530

    4th commit

--- ecomsi/settings.py
+++ ecomsi/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'category.context_processors.menu_links'
+
 ]
 
 ROOT_URLCONF = 'ecomsi.urls'
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                 'category.context_processors.menu_links',
             ],
         },
     },
